c7755d165b2aa714a9ec79daa96d11b53adeba9b
dcdmm <dcdmmcomeon@gmail.com>
2022-01-31 17:00:16 +0800

    最后完善

--- MySQL/数据定义/自增长.sql
+++ MySQL/数据定义/自增长.sql
@@ -9,7 +9,7 @@ drop table if exists auto_incre;
 create table auto_incre
 (
     # There can be only one AUTO_INCREMENT column per table, it must be indexed, and it cannot have a DEFAULT value.
-    # 默认主键约束
+    # 自增长列默认主键约束
     # 必须是数值类型
     id   double key auto_increment,
     NAME varchar(20)
